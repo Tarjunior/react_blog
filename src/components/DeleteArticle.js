0117297227c2aaf645577a4da0ec34bee95e267a
unknown <Tarjunior>
2021-12-28 16:37:19 +0100

    Mise en place du projet blog

---
+++ src/components/DeleteArticle.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import axios from 'axios';
+
+const DeleteArticle = ({ id }) => {
+
+    const handleDelete = () => {
+
+        axios.delete('http://localhost:3003/articles/' + id);
+        window.location.reload();
+    }
+
+    return (
+        <button onClick={ () => {
+
+            if(window.confirm("Voulez vous supprimer ?")) {
+                handleDelete()
+            }
+        }}
+        >
+            Supprimer
+
+        </button>
+    );
+};
+
+export default DeleteArticle;
